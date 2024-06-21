6aadf33ace501b29c0978f2b2d9980446ef9d9a5
clarkm4705 <168484001+clarkm4705@users.noreply.github.com>
2024-06-21 13:55:10 -0500

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,16 +1,19 @@
 function travelSearch(event) {
     event.preventDefault();
+    const searchString = document.getElementById('SearchBox');
+    console.log(searchString.toLowerCase());
+    
     fetch(./travel_recommendation_api.json)
     .then(response => response.json())
     .then(data => {
-        const recommentationInfo = document.getElementById('recommendationInfo');
+        const recommendationInfo = document.getElementById('travelsearchresults');
         console.log(JSON.stringify(data));
-        recommendationInfo.innerHTML = `<div><p>JSON results</p></div>`;
+        recommendationInfo.innerHTML = `<p>JSON results</p>`;
         
     })
     .catch(error => {
         console.error('Error fetching recommendations', error);
-        const recommendationInfo = document.getElementById('recommendationInfo');
+        const recommendationInfo = document.getElementById('travelsearchresults');
         recommendationInfo.innerHTML = `<p>Failed to fetch recommendations. Please try again or <a href = "./contact.html">Contact Us</a> for help.</p>`;
 
     });
