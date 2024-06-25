a0dcecc393c0c3a9f0ad122b93269adb840400c8
clarkm4705 <168484001+clarkm4705@users.noreply.github.com>
2024-06-25 13:21:10 -0500

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,20 +2,22 @@ function travelSearch(event) {
     event.preventDefault();
     const searchString = document.getElementById('SearchBox');
     console.log(searchString.toLowerCase());
-    
-    fetch(./travel_recommendation_api.json)
-    .then(response => response.json())
-    .then(data => {
-        const recommendationInfo = document.getElementById('travelsearchresults');
-        console.log(JSON.stringify(data));
-        recommendationInfo.innerHTML = `<p>JSON results</p>`;
-        
-    })
-    .catch(error => {
-        console.error('Error fetching recommendations', error);
-        const recommendationInfo = document.getElementById('travelsearchresults');
-        recommendationInfo.innerHTML = `<p>Failed to fetch recommendations. Please try again or <a href = "./contact.html">Contact Us</a> for help.</p>`;
+    const apiUrl = 'https://clarkm4705.github.io/GetAwayTravel/travel_recommendation_api.json';
+    fetch(apiUrl)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            const recommendationInfo = document.getElementById('travelsearchresults');
+            console.log(JSON.stringify(data));
+            recommendationInfo.innerHTML = `<p>JSON results</p>`;
+            
+        })
+        .catch(error => {
+            console.error('Error fetching recommendations', error);
+            const recommendationInfo = document.getElementById('travelsearchresults');
+            recommendationInfo.innerHTML = `<p>Failed to fetch recommendations. Please try again or <a href = "./contact.html">Contact Us</a> for help.</p>`;
 
     });
 }  
-document.getElementById('navbarsearchform').addEventListener('submit', travelSearch);
+document.getElementById('SearchButton').addEventListener('submit', travelSearch);
